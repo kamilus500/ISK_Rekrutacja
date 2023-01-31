6c1d298e502e6f4dc8f10d944d84095c676d7140
Kamil Kurzeja <asdqw@gmail.com>
2023-01-31 18:31:34 +0100

    changes

--- ISK_Rekrutacja/DAL/Client.cs
+++ ISK_Rekrutacja/DAL/Client.cs
@@ -1,9 +1,14 @@
-﻿namespace ISK_Rekrutacja.DAL
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ISK_Rekrutacja.DAL
 {
     public class Client
     {
         public Guid Id { get; set; }
+
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
         public int Age { get; set; }
     }
