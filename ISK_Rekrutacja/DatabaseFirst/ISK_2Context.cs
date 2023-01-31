dfd225212a41c69f470d2c6e90618a0e69749b11
Kamil Kurzeja <asdqw@gmail.com>
2023-01-31 18:05:09 +0100

    create context by database First

---
+++ ISK_Rekrutacja/DatabaseFirst/ISK_2Context.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ISK_Rekrutacja.DatabaseFirst
+{
+    public partial class ISK_2Context : DbContext
+    {
+        public ISK_2Context()
+        {
+        }
+
+        public ISK_2Context(DbContextOptions<ISK_2Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Client> Clients { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-CLE8MOP;Initial Catalog=ISK_2;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>(entity =>
+            {
+                entity.ToTable("Client");
+
+                entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+
+                entity.Property(e => e.LastName).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
